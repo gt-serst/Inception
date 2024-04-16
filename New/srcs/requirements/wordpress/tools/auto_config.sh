da1e6714e27ed86adbec2758b777aaa6fb70b849
test user <gt-serst@student.s19.be>
2024-04-16 14:13:16 -0400

    site is working

--- New/srcs/requirements/wordpress/tools/auto_config.sh
+++ New/srcs/requirements/wordpress/tools/auto_config.sh
@@ -2,12 +2,39 @@
 
 sleep 10
 
-wp config create --allow-root \
-	--dbname=$MYSQL_DATABASE \
-	--dbuser=$MYSQL_USER \
-	--dbpass=$MYSQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+# if [ -f "/var/www/html/wordpress/wp-config.php" ]; then
+# 	rm -rf /var/www/html/wordpress/wp-config.php
+# fi
 
-wp core install --url=gt-serst.42.fr --title="Hello World!" --admin_user=$MYWP_USER --admin_password=$MYWP_PASSWORD --admin_email=$MYWP_EMAIL
+if [ ! -d "/var/www/html/wordpress" ]; then
+	mkdir -p /var/www/html/wordpress
+fi
 
-wp user create $MYWP_USER $MYWP_EMAIL --role=author --user_pass=$MYWP_PASSWORD
+cd /var/www/html/wordpress
+rm -rf /var/www/html/wordpress/*
+
+wp core download	--allow-root
+
+wp config create	--allow-root \
+					--dbname=$MYWP_DATABASE \
+					--dbuser=$MYWP_USER \
+					--dbpass=$MYWP_PASSWORD \
+					--dbhost=mariadb:3306 \
+					--path='/var/www/html/wordpress/'
+
+wp core install	--allow-root \
+				--url=$MYWP_URL \
+				--title="Hello World !" \
+				--admin_user=$MYWP_ADMIN_USER \
+				--admin_password=$MYWP_ADMIN_PASSWORD \
+				--admin_email=$MYWP_ADMIN_EMAIL
+
+wp user create	--allow-root \
+				$MYWP_SUB_USER \
+				$MYWP_SUB_EMAIL \
+				--role=$MYWP_SUB_ROLE \
+				--user_pass=$MYWP_SUB_PASSWORD
+
+ln -s $(find /usr/sbin -name 'php-fpm*') /usr/bin/php-fpm
+
+exec "$@"
